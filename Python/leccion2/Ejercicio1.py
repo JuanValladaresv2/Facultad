4f1322c9f1606805bb4513753acfed75e05aaab8
Juan Valladares <juan_1@outlook.es>
2022-06-09 09:47:56 -0300

    Ingresamos otra actualizacion de los archivos de Python

---
+++ Python/leccion2/Ejercicio1.py
@@ -0,0 +1,19 @@
+## Clase 6
+"""
+Ejercicio1:
+Debemos plasmar la expresión en una expresión algoritmica, osea hacerlo en código
+"""
+"""
+a = float(input("digite el valor de a: "))
+b= float(input("digite el valor de b: "))
+c = float(input("digite el valor de c: "))
+resultado = (a ** 3* (b 2 - 2 * a * c)) / (2 * b)
+print(f"El resultado es: {resultado}")
+"""
+condicion = True
+#if condicion:
+#    print("condicion Verdadera")
+#else:
+#    print("condicion falsa")
+
+print("Condición Verdadera") if condicion else print("Condicion Falsa")
--- Python/leccion2/main.py
+++ Python/leccion2/main.py
@@ -1,6 +1,7 @@
 #Clase 6
 
 # En esta clase veremos la sentencia if/else
+"""
 condicion=10
 if condicion == True:
     print("Condicion verdadera")
@@ -8,4 +9,16 @@ elif condicion == False:
     print("Condicion falsa")
 else:
     print("Condicion sin especificar")
+"""
 
+num = int(input("Digite un número en el rango de 1 al 3 "))
+numTexto = ""
+if num ==1:
+    numTexto = "Número uno"
+elif num == 2:
+    numTexto = "Número dos"
+elif num == 3:
+    numTexto = "Número tres"
+else:
+    numTexto = "Has ingresado un número fuera de rango"
+print(f"El número ingresado es:  {num} - {numTexto} ")
