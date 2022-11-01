65d41b67a2ba35393d277645d46e6a15cbeb403b
Juan Valladares <juan_1@outlook.es>
2022-11-01 16:58:24 -0300

    Commit de las clases de Java y Python

---
+++ Python/Leccion8/Vehiculo.py
@@ -0,0 +1,57 @@
+class Vehiculo:
+    """
+    Definir una clase padre llamada Vehiculo y dos clase hijas llamadas
+    Auto y Bicicleta, las cuales heredan de la clase padre Vehiculo. La clase
+    padre debe tener los siguientes atributos y métodos:
+    Vehiculo(clase padre)
+    -Atributos(color, ruedas)
+    -Métodos(__init__(color y ruedas) y __str__())
+
+    Auto(clase hija de Vehiculo)
+    -Atributos(velocidad(km/h))
+    -Métodos(__init__(color, ruedas, velocidad) y __srt__())
+
+    Bicicleta(clase hija de Vehiculo)
+    -Atributos(tipo(Urbana/montaña/etc.)
+    -Métodos(__init__(color, ruedas, tipo) y __srt__())
+
+    Crear un objeto de cada clase
+    """
+
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return "Color: " + self.color + ", Ruedas: " + str(self.ruedas)
+
+
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+
+    def __str__(self):
+        return super().__str__() + ", velocidad(KM/H): " + str(self.velocidad)
+
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo
+
+    def __str__(self):
+        return super().__str__()+ ", Tipo: "+self.tipo
+
+
+# Primer objeto de la clase padre Vehiculo
+vehiculo = Vehiculo("Blanco", 4)
+print(vehiculo)
+
+# Segundo objeto de la clase padre Vehiculo
+auto = Auto("Amarillo", 4, 120)
+print(auto)
+
+# Tercer objeto de la clase padre vehículo
+bici = Bicicleta("Azul", 2, "Urbana")
+print(bici)
