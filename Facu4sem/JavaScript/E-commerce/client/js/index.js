a50306884a465c8b094d7087e2faff2ba2d9084b
JuanValladaresv2 <juan_1@outlook.es>
2023-09-08 14:21:30 -0300

    Clase 3 4SEM JavaScript
    E-commerce Básico Parte 03

--- Facu4sem/JavaScript/E-commerce/client/js/index.js
+++ Facu4sem/JavaScript/E-commerce/client/js/index.js
@@ -1,12 +1,13 @@
 const shopContent = document.getElementById("shopContent");
 const cart = []; //Este es nuestro carrito, un array vacío
 
-productos.forEach((product) =>{
+productos.forEach((product) => {
     const content = document.createElement("div");
+    content.className = "card";
     content.innerHTML = `
     <img src="${product.img}">
     <h3>${product.productName}</h3>
-    <p>${product.price} $</p>
+    <p class="price">${product.price} $</p>
     `;
     shopContent.append(content);
 
@@ -16,13 +17,22 @@ productos.forEach((product) =>{
     content.append(buyButton);
 
     buyButton.addEventListener("click",()=>{
-        cart.push({
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quanty: product.quanty,
-            img: product.img,
-        })
-        console.log(cart)
-    })
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+        if(repeat){
+            cart.map((prod)=> {
+                if(prod.id === product.id){
+                    prod.quanty++
+                }
+            });
+        }else{
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quanty: product.quanty,
+                img: product.img,
+            });
+        }
+    });
 });
